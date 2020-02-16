1eb6bf18c9f8fc5598c4482f080c19ca5c12e3fa
ait <ait@ex.in>
2020-02-16 20:28:33 +0900

    add query

--- pages/mypage/_id.vue
+++ pages/mypage/_id.vue
@@ -3,7 +3,9 @@
   <section>
     <h1>マイページ</h1>
     <!-- <p>こんにちは、「{{ $route.params.id }}」さん！</p> -->
-    <p>こんにちは、「{{ id }}」さん！</p>
+    <p>ここは「{{ id }}」のページ</p>
+    <p>こんにちは、「{{ name }}」さん！</p>
+    <p>example: /mypage/設定変更?name=山田太郎</p>
     <a href="https://www.i-ryo.com/entry/2019/11/27/062247" target="_blank">参考</a>
   </section>
 
@@ -17,9 +19,15 @@ export default {
   computed: {
     id: function() {
       // return this.$route.params.id
-      let id = this.$route.params.id != undefined ? this.$route.params.id : '仮の名前'
+      // ↓ page path
+      let id = this.$route.params.id != undefined ? this.$route.params.id : 'ページ名'
       return id
-    }
+    },
+    name: function() {
+      // query param
+      let name = this.$route.query.name != undefined ? this.$route.query.name : '仮の名前'
+      return name
+    },
   }
 }
 </script>
