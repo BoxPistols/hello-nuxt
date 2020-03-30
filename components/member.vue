e8c8443416b3218646c84b53b431beff63e16447
ait <ait@ex.in>
2020-03-30 18:25:57 +0900

    add re Vuex

--- components/member.vue
+++ components/member.vue
@@ -1,40 +1,44 @@
 <template>
   <div id="">
-  <h2>{{ teamName }}</h2>
-  <ul v-for="data in datas" :key="data.id">
-  <li>{{ data.name }}</li>
-  <li>{{ data.id }}</li>
-  <li :class="pC(data.position)">{{ data.position }}</li>
-  <li :class="hC(data.height)">{{ data.height }}</li>
-  </ul>
+    <h2>{{ teamName }}</h2>
+
+    <ul v-for="d in list" :key="d.id">
+      <li>{{ d.name }}</li>
+      <li>{{ d.id }}</li>
+      <li :class="pC(d.position)">{{ d.position }}</li>
+      <li :class="hC(d.height)">{{ d.height }}</li>
+    </ul>
+
   </div>
 </template>
-
 <script>
-export default {
-  name: "",
-  props: {
-    datas: Object,
-    teamName: String
-  },
-  methods: {
-    hC(height) {
-      console.log(height)
-      if (height > 170) {
-        console.log("hi")
-        return 'hi'
-      } else {
-        console.log("no")
-        return 'low'
-      }
+  export default {
+    name: "",
+    props: {
+      list: {
+        type: Array,
+        require  : false,
+      },
+      teamName: String
     },
-    pC(position){
-      if(position === "族車"){
-        return 'gold'
+    methods: {
+      hC(height) {
+        console.log(height)
+        if (height > 170) {
+          console.log("hi")
+          return 'hi'
+        } else {
+          console.log("no")
+          return 'low'
+        }
+      },
+      pC(position) {
+        if (position === "族車") {
+          return 'gold'
+        }
       }
     }
   }
-}
 </script>
 
 <style lang="sass" scoped>
--- store/index.js
+++ store/index.js
@@ -2,14 +2,24 @@ import Vuex from "vuex"
 
 const store = () => {
   return new Vuex.Store({
+
     state: function () {
-      return {msg: 'Hello Vuex'}
+      return {
+        msg: 'Hello Vuex',
+        appNum: 0
+      }
     },
+
     mutations: {
       updateMsg: function (state) {
         state.msg = 'UpDate Mutation'
+      },
+      changeNum(state, val){
+        state.msg = 'changeNum Mutation'
+        state.appNum = state.appNum + val
       }
     }
+
   })
 }
 export default store
